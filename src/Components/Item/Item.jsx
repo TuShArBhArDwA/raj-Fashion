d6b3bbf728ab59c87339bb17bacd101f34f99c4f
Tushar Bhardwaj <tusharbhardwaj2617@gmail.com>
2024-10-28 16:25:52 +0530

    pre

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import './Item.css'
 const Item = (props) => {
   return (
-    <div>
+    <div className='item'>
         <img src={props.image} alt="" />
         <p>{props.name}</p>
         <div className="items-prices">
             <div className="items-price-new">
-                {props.new_price}
+            ₹ {props.new_price}
             </div>
             <div className="item-price-old">
-                {props.old_price}
+            ₹ {props.old_price}
             </div>
         </div>
     </div>

43a130df28a83930721e912479e6bc74fb63030c
Tushar Bhardwaj <tusharbhardwaj2617@gmail.com>
2024-11-18 11:58:24 +0530

    final touch

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -12,29 +12,34 @@ const Footer = () => {
             <p>Raj Fashion</p>
         </div>
         <ul className="footer-links">
-            <li>Company</li> 
-            <li>Product</li>
-            <li>Offices</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li><a href="https://maps.app.goo.gl/CBm7K16gaa9vDnk5A">Company</a></li>
+            <li><a href="#">Product</a></li>
+            <li><a href="https://maps.app.goo.gl/CBm7K16gaa9vDnk5A">Offices</a></li>
+            <li><a href="https://www.google.co.in/maps/place/Raj+Fashion/@29.7100663,75.8990327,17z/data=!4m16!1m8!3m7!1s0x3911994975db6673:0xee0f8c903867c7c5!2sRaj+Fashion!8m2!3d29.7100626!4d75.8990555!10e5!16s%2Fg%2F11h7tdnp6l!3m6!1s0x3911994975db6673:0xee0f8c903867c7c5!8m2!3d29.7100626!4d75.8990555!10e1!16s%2Fg%2F11h7tdnp6l?entry=ttu&g_ep=EgoyMDI0MTExMy4xIKXMDSoASAFQAw%3D%3D">About</a></li>
+            <li><a href="mailto:hardikgarg987@gmail.com?subject=Inquiry&body=Hello, I would like more information about your products and services.">Contact</a></li>
         </ul>
         <div className="footer-social-icon">
             <div className="footer-icons-container">
-                <img src={instagram_icon} alt=""/>
+                <a href="https://instagram.com" target="_blank"  rel="noreferrer">
+                    <img src={instagram_icon} alt="Instagram"/>
+                </a>
+            </div>
+            <div className="footer-icons-container">
+                <a href="https://pinterest.com" target="_blank"  rel="noreferrer">
+                    <img src={pintester_icon} alt="Pinterest"/>
+                </a>
+            </div>
+            <div className="footer-icons-container">
+                <a href="https://whatsapp.com" target="_blank"  rel="noreferrer">
+                    <img src={whatsapp_icon} alt="WhatsApp"/>
+                </a>
+            </div>
         </div>
-        <div className="footer-icons-container">
-                <img src={pintester_icon} alt=""/>
-        </div>
-        <div className="footer-icons-container">
-                <img src={whatsapp_icon} alt=""/>
+        <div className="footer-copyright">
+            <hr/>
+            <p>Copyright @ 2024 - All Right Reserved.</p>
         </div>
     </div>
-    <div className="footer-copyright">
-        <hr/>
-        <p>Copyright @ 2024 - All Right Reserved.</p>
-    </div>
-    </div>
-
   )
 }
 
--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom'
 const Item = (props) => {
   return (
     <div className='item'>
-        <Link to={`/product/₹{props.id}`}><img onClick={window.scrollTo(0,0)} src={props.image} alt="" /></Link>
+        <Link to={`/product/${props.id}`}><img onClick={window.scrollTo(0,0)} src={props.image} alt="" /></Link>
         <p>{props.name}</p>
         <div className="items-prices">
             <div className="items-price-new">
-            ₹{props.new_price}
+            ₹ {props.new_price}
             </div>
             <div className="item-price-old">
-            ₹{props.old_price}
+            ₹ {props.old_price}
             </div>
         </div>
     </div>
